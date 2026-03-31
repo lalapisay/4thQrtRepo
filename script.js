3983d47a2294e1d4433db0ddf12768f0267ef284
lalapisay <b2029laepauig@pshs.edu.ph>
2026-03-31 22:40:15 +0800

    commit

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function plotPoint() {
-    console.log(x0, y0, x, y) // checks the arguments passed to this function
+    console.log(Number(x0.value), Number(y0.value), Number(x.value), Number(y.value)) // checks the arguments passed to this function
     in1.innerHTML = x0.value + " " + y0.value + " " + x.value + " " + y.value; //this is correction 2
     var point = document.createElement('div');  
-    point.className = 'point';
+    point.id = 'point';
     /* control where to place the div on the screen using left and bottom of position:absolute */
-    point.style.left = (x - x0 + 200 - 5) + 'px'; // subtract half the width of the point to center it by changing left css property
-    point.style.bottom = (y - y0 + 200 - 5) + 'px'; // subtract half the height of the point to center it by changing bottom css property
     document.getElementById('coordinatePlane').appendChild(point);
-
+    point.style.left = (Number(x.value) - Number(x0.value) + 200 - 5) + 'px'; // subtract half the width of the point to center it by changing left css property
+    point.style.bottom = (Number(y.value) - Number(y0.value) + 200 - 5) + 'px'; // subtract half the height of the point to center it by changing bottom css property
+    
     if (Number(x.value) > Number(x0.value) && Number(y.value) > Number(y0.value)){
       out1.innerHTML = "NE"
     }
