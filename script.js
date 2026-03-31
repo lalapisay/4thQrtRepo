9c586be01f0f39f217de1067b304f3d59e5e91d6
lalapisay <b2029laepauig@pshs.edu.ph>
2026-03-31 21:36:52 +0800

    ayaw nya magsave

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 function plotPoint() {
     console.log(x0, y0, x, y) // checks the arguments passed to this function
-    in1.innerHTML = x0 + " " + y0 + " " + x + " " + y;
+    in1.innerHTML = x0.value + " " + y0.value + " " + x.value + " " + y.value; //this is correction 2
     var point = document.createElement('div');  
     point.className = 'point';
     /* control where to place the div on the screen using left and bottom of position:absolute */
-    point.styleleft = (x - x0 + 200 - 5) + 'px'; // subtract half the width of the point to center it by changing left css property
-    point.stylebottom = (y - y0 + 200 - 5) + 'px'; // subtract half the height of the point to center it by changing bottom css property
+    point.style.left = (x - x0 + 200 - 5) + 'px'; // subtract half the width of the point to center it by changing left css property
+    point.style.bottom = (y - y0 + 200 - 5) + 'px'; // subtract half the height of the point to center it by changing bottom css property
     document.getElementById('coordinatePlane').appendChild(point);
 
-    if (x > x0 && y > y0){
+    if (Number(x.value) > Number(x0.value) && Number(y.value) > Number(y0.value)){
       out1.innerHTML = "NE"
     }
-    else if (x < x0 && y < y0){
+    else if (Number(x.value) < Number(x0.value) && Number(y.value) < Number(y0.value)){
       out1.innerHTML = "SO"
     }
-    else if (x > x0 && y < y0){
+    else if (Number(x.value) > Number(x0.value) && Number(y.value) < Number(y0.value)){
       out1.innerHTML = "SE"
     }
-    else if (x < x0 && y > y0){
+    else if (Number(x.value) < Number(x0.value) && Number(y.value) > Number(y0.value)){
       out1.innerHTML = "NO"
     }
-    else{
+    else if (Number(x.value) === Number(x0.value) || Number(y.value) === Number(y0.value)){
       out1.innerHTML = "divisa"
     }
   } //this is correction 1, missing bracket
